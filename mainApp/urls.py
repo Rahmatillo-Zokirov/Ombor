3d5e7f0614fc2f970e839d28278bcee0833ed0e1
rahmatillo zokirov <rahmatillozokirovgithub@gmail.com>
2024-05-10 21:08:24 +0500

    as

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('bolimlar/', BolimView.as_view(), name='bolimlar'),
     path('mahsulotlar/', MahsulotlarView.as_view(), name='mahsulotlar'),
     path('mijozlar/', MijozlarView.as_view(), name='mijozlar'),
-    path('mahsulot/<int:pk>/tahrirlash/', MahsulotTahrirlashView.as_view(), name='mahsulot_tahrirlash'),
-    path('mahsulot/<int:pk>/ochirish/', MahsulotOchirishView.as_view(), name='mahsulot_ochirish'),
+    path('mahsulotlar/<int:pk>/tahrirlash/', MahsulotTahrirlashView.as_view(), name='mahsulot_tahrirlash'),
+    path('mahsulotlar/<int:pk>/ochirish/', MahsulotDeleteView.as_view(), name='mahsulot_ochirish'),
     path('mijozlar/', MijozlarView.as_view(), name='mijozlar')
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import DetailView
@@ -29,6 +30,21 @@ class MahsulotlarView(View):
             return render(request, 'mahsulotlar.html', content)
         return redirect('login')
 
+    def post(self, request):
+        if request.user.is_authenticated:
+            Mahsulot.objects.create(
+                tarqatuvchi=request.user,
+                nom = request.POST.get('nom'),
+                brand = request.POST.get('brand'),
+                narx1 = request.POST.get('narx1'),
+                narx2 = request.POST.get('narx2'),
+                miqdor = request.POST.get('miqdor'),
+                olchov = request.POST.get('olchov'),
+                sana = request.POST.get('sana'),
+            )
+            return redirect('mahsulotlar')
+        return redirect('login')
+
 
 
 class MahsulotTahrirlashView(View):
@@ -38,19 +54,15 @@ class MahsulotTahrirlashView(View):
         return render(request, 'mahsulot-tahrirlash.html', context)
 
     def post(self, request, pk):
-        mahsulot = get_object_or_404(Mahsulot, pk=pk)
-        mahsulot.nom = request.POST.get('name')
-        mahsulot.brand = request.POST.get('brand_name')
-        mahsulot.narx1 = request.POST.get('price')
-        mahsulot.miqdor = request.POST.get('amount')
-        mahsulot.save()
-        return redirect('mahsulot_tahrirlash', pk=pk)
-
-class MahsulotOchirishView(View):
-    def post(self, request, mahsulot_id):
-        mahsulot = get_object_or_404(Mahsulot, pk=mahsulot_id)
-        mahsulot.delete()
-        return redirect('mahsulotlar')
+        if request.user.is_authenticated:
+            mahsulot = get_object_or_404(Mahsulot, pk=pk)
+            mahsulot.nom = request.POST.get('name')
+            mahsulot.brand = request.POST.get('brand_name')
+            mahsulot.narx1 = request.POST.get('price')
+            mahsulot.miqdor = request.POST.get('amount')
+            mahsulot.save()
+            return redirect('mahsulotlar',)
+        return redirect('login')
 
 
 class MijozlarView(View):
@@ -58,4 +70,46 @@ class MijozlarView(View):
         if request.user.is_authenticated:
             mijozlar = Mijoz.objects.filter(tarqatuvchi=request.user)
             return render(request, 'mijozlar.html', {'mijozlar': mijozlar})
+        return redirect('login')
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            Mijoz.objects.create(
+                tarqatuvchi=request.user,
+                ism = request.POST.get('ism'),
+                dokon = request.POST.get('dokon'),
+                tel = request.POST.get('tel'),
+                manzil = request.POST.get('manzil'),
+                qarz = request.POST.get('qarz'),
+            )
+            return redirect('mijozlar')
+        return redirect('login')
+
+
+class MahsulotDeleteView(View):
+    def get(self, request, pk):
+        if request.user.is_authenticated:
+            mahsulot = Mahsulot.objects.get(pk=pk)
+            if mahsulot.tarqatuvchi == request.user:
+                mahsulot.delete()
+            return redirect('mahsulotlar')
+        return redirect('login')
+
+
+
+
+class QidirishView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            query = request.GET.get('q')  # Qidirish so'rovi
+            if query:
+                mahsulotlar = Mahsulot.objects.filter(
+                    Q(nom__icontains=query) | Q(brand__icontains=query)  # Mahsulot nomi yoki brendi bo'yicha qidirish
+                )
+            else:
+                mahsulotlar = Mahsulot.objects.filter(tarqatuvchi=request.user)
+            content = {
+                'mahsulotlar': mahsulotlar
+            }
+            return render(request, 'mahsulotlar.html', content)
         return redirect('login')
